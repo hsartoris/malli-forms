10587f5306a07f0282cee94b525a07b2e0841950
Hayden Sartoris <hsartoris@gmail.com>
2021-12-10 14:28:44 -0500

    pprint helper

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -1,3 +1,4 @@
+#_:clj-kondo/ignore
 (ns user
   (:require
     [clojure.data :refer [diff]]
@@ -9,3 +10,8 @@
     [malli.transform :as mt]
     [malli.util :as mu])
   (:use clojure.repl))
+
+;; s/(pprint (m\/form *1))/(pp)/g
+(defmethod pp/simple-dispatch malli.core.Schema
+  [schema]
+  (pp/simple-dispatch (m/form schema)))

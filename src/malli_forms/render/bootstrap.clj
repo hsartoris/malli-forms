f7d395b011150bb979d60cae84278d08c49f488d
Hayden Sartoris <hsartoris@gmail.com>
2022-12-09 12:02:42 -0500

    working on bootstrap renderer

---
+++ src/malli_forms/render/bootstrap.clj
@@ -0,0 +1,92 @@
+(ns malli-forms.render.bootstrap
+  (:require
+    [clojure.string :as str]
+    [malli-forms :as-alias mf]
+    [malli-forms.util :as util]))
+
+(defn- props->attrs
+  "Wraps util/props->attrs to add common error detection"
+  [{:keys [errors] :as spec}]
+  ;; TODO: notes
+  (let [attrs (util/props->attrs spec)]
+    (cond-> attrs
+      (seq errors) (-> (update :class str " is-invalid")
+                       ;; TODO: very bad solution
+                       (assoc :aria-describedby (str (or (:id spec)
+                                                         (str "mf-" (util/path->name (:path spec))))
+                                                     "-feedback"))))))
+
+(defn- error-spans
+  "Get a seq of error spans from a spec"
+  [{:keys [errors] :as spec}]
+  (when (seq errors)
+    [:div.invalid-feedback {:id (str (or (:id spec)
+                                         (str "mf-" (util/path->name (:path spec))))
+                                     "-feedback")}
+     (str/join ", " (map :message errors))]))
+
+(defmulti render "Bootstrap 5 renderer" :type)
+
+(defn- labeled-input
+  [{:keys [id label prefix suffix] :as field-spec}]
+  (let [group? (or prefix suffix)
+        field [:input.form-control (util/props->attrs field-spec)]
+        input (if-not group?
+                field
+                [:div.input-group
+                 (when prefix [:span.input-group-text prefix])
+                 field
+                 (when suffix [:span.input-group-text suffix])
+                 ;; TODO: errors
+                 ])]
+    [:div.mb-3
+     (when label
+       [:label.form-label {:for id} label])
+     input
+     (error-spans field-spec)
+     ]))
+
+(defmethod render :default [spec] (labeled-input spec))
+
+(defmethod render :radio
+  [{:keys [options label value path] :as spec}]
+  [:fieldset
+   (when label [:label.form-label {:for (:name spec)} label])
+   (for [option options
+         :let [label (util/value->label option)
+               id (util/path->name (conj path option))
+               sel? (= option value)]]
+     [:div.form-check
+      [:input.form-check-input
+       (-> (props->attrs spec) (assoc :checked sel?, :id id, :value (str option)))]
+      [:label.form-check-label {:for id} label]])])
+
+(defmethod render :select
+  [{:keys [options label id] :as spec}]
+  [:div.mb-3
+   (when label [:label.form-label {:for id} label])
+   [:select.form-select
+    (-> spec props->attrs (dissoc :label :options :value))
+    (map #(vector :option % (:value %)) options)]
+   (error-spans spec)])
+
+(defmethod render :submit [spec]
+  [:button.btn.btn-primary.mt-3 (props->attrs spec) (or (util/label spec) (:value spec))])
+
+(defmethod render :button [spec]
+  [:button.btn.btn-secondary (assoc (props->attrs spec) :type "submit") ;; consequences?
+   (or (util/label spec) (:value spec))])
+
+(defmethod render ::mf/collection
+  [{:keys [children] :as spec}]
+  (list
+    [:fieldset
+     (props->attrs spec)
+     (when-some [l (util/label spec)] [:legend l])
+     (seq children)]
+   (error-spans spec)))
+  
+
+(defmethod render ::mf/form
+  [{:keys [children] :as spec}]
+  [:form (util/props->attrs spec) (seq children)])
--- test/malli_forms/reitit_test.clj
+++ test/malli_forms/reitit_test.clj
@@ -21,36 +21,64 @@
   [data]
   [:pre (with-out-str (pprint data))])
 
+(def ^:private some-routes
+  [""
+   {:middleware [(fn [handler]
+                   (fn [req]
+                     (let [res (handler req)]
+                       (if (map? res) res {:status 200, :body res}))))
+                 mf.mw/schema-endpoint-reitit]}
+   ["/pools/new"
+    {::mf/schema asn-pool-schema
+     :handler mf.mw/stub-handler}]
+   ["/enums"
+    {::mf/schema [:enum :a "a"]
+     :handler mf.mw/stub-handler}]
+   ["/math"
+    ["/echo"
+     {::mf/schema [:map
+                   [::my-number int?]]
+      :handler    mf.mw/stub-handler}]
+    ["/inc"
+     {::mf/schema int?
+      :handler #(-> % ::mf/value inc)
+      :middleware [(fn [handler]
+                     (fn [req]
+                       [:span (handler req)]))]}]
+    ["/sum"
+     ;; won't actually respond to form submission
+     {:get {:handler (fn [_]
+                       (mf/render-form [:set number?]))}}]]
+   ["/activities/new"
+    {::mf/schema activity
+     :handler mf.mw/stub-handler}]])
+
+
 (def router
   "Reitit router for testing above schema"
   (ring/router
     [[""
-      ["/pools/new"
-       {:name     ::new-pool
-        ::mf/schema asn-pool-schema
-        :handler mf.mw/stub-handler}]
-      ["/enums"
-       {::mf/schema [:enum :a "a"]
-        :handler mf.mw/stub-handler}]
-      ["/math"
-       ["/echo"
-        {::mf/schema [:map
-                      [::my-number int?]]
-         :handler    mf.mw/stub-handler}]
-       ["/inc"
-        {::mf/schema int?
-         :handler #(-> % ::mf/value inc)
-         :middleware [(fn [handler]
-                        (fn [req]
-                          [:span (handler req)]))]}]
-       ["/sum"
-        ;; won't actually respond to form submission
-        {:get {:handler (fn [_]
-                          (mf/render-form [:set number?]))}}]]
-      ["/activities/new"
-       {:name    ::new-activity
-        ::mf/schema activity
-        :handler mf.mw/stub-handler}]]]
+      {:middleware [(fn [handler]
+                      (fn [req]
+                        (-> (handler req)
+                            (update :body #(rum/render-static-markup [:html [:body [:main %]]])))))]}
+      some-routes]
+     ["/bootstrap"
+      {::mf/render bs/render
+       :middleware [(fn [handler]
+                      (fn [req]
+                        (-> (handler req)
+                            (update :body #(rum/render-static-markup
+                                             [:html
+                                              [:head
+                                               [:link {:href "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+                                                       :rel "stylesheet"}]
+                                               ]
+                                              [:body
+                                               [:main.container
+                                                [:div.row [:div.col-s12 %]]]]])))))]}
+      some-routes]]
+
     {:data {:middleware [parameters/parameters-middleware
                          mf.mw/remove-empty-params
                          wrap-nested-params
@@ -58,16 +86,7 @@
                            (fn [req]
                              (-> (handler req)
                                  (assoc-in [:headers "content-type"] "text/html")
-                                 (update :body #(rum/render-static-markup
-                                                  [:html [:body [:main %]]])))))
-                         (fn [handler]
-                           (fn [req]
-                             (let [res (handler req)]
-                               (if (map? res)
-                                 res
-                                 {:body res
-                                  :status 200}))))
-                         mf.mw/schema-endpoint-reitit
+                                 (update :body #(str "<!DOCTYPE html>\n" %)))))
                          ]}}))
 
 (def ^:private app (ring/ring-handler router))
